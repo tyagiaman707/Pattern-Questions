acf17ccf0e913cb45b4c7bdc1addd4ac5f03fc37
AMAN TYAGI <95289326+tyagiaman707@users.noreply.github.com>
2025-12-04 10:19:03 +0530

    Add files via upload

---
+++ question2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class question2 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        for(int i=1;i<=a;i++){
+            for(int j=1;j<=b;j++){
+                if(i==1||i==a||j==1||j==b){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }System.out.println();
+        }
+    }
+}
